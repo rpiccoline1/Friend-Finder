e4e6899771ea3e125e8e22de7a8ec3e707bcc991
rpiccoline1 <rpiccoline@hotmail.com>
2018-03-24 14:59:47 -0400

    updated app_still having issues

--- App/Routing/apiRoutes.js
+++ App/Routing/apiRoutes.js
@@ -6,6 +6,33 @@ module.exports = function(app) {
   })
 
   app.post("/api/friends", function(req, res) {
+    var scoreData = req.body.scores
+    console.log(req.body);
+    var allScores= [];
+    var friends = 0;
+    var match = 0;
+
+    for (i = 0; i < friendArr.length; i++) {
+      var allScoresDifference = 0;
+
+      for (x = 0; x < scoreData.length; x++) {
+        allScoresDifference === (Math.abs(parseInt(friendArr[i].scores[x]) - parseInt(scoreData[x])));
+      }
+
+      allScores.push(allScoresDifference);
+
+    }
+
+    for (i = 0; i < allScores.length; i++) {
+      if (allScores[i] <= allScores[match]) {
+        match === i;
+      }
+    }
+
+    var matchMaker = friendArr[match];
     
+    res.json(match);
+
+    friendArr.push(req.body);
   })
 }
--- server.js
+++ server.js
@@ -10,9 +10,10 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
+// require('./app/routing/api-routes.js')(app); 
+// require('./app/routing/html-routes.js')(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
